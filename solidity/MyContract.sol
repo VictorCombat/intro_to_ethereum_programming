1b5f34c1ce5bc06e2e11a29da01771f751086428
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-06 22:06:52 +0100

    :sparkles: Solidity #2

--- solidity/MyContract.sol
+++ solidity/MyContract.sol
@@ -1,17 +1,48 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.5.1;
 
 contract MyContract {
-    string value;
+    // =======================
+    // ======== TYPES ========
+    // =======================
+    string public stringValue = "myString";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint8 public myUint8 = 10;
+    uint256 public myUint256 = 888889949;
+    
+    // ========================
+    // ========= ENUM =========
+    // ========================
+    enum State { Waiting, Ready, Active }
+    State public state;
     
     constructor() public {
-        value = "myvalue";
+        state = State.Waiting;
     }
     
-    function get() public view returns(string){
-        return value;
+    function activate() public {
+        state = State.Active;
     }
     
-    function set(string _value) public {
-        value = _value;
+    function isActive() public view returns(bool) {
+        return state == State.Active;
+    }
+
+    // ======================
+    // ======= STRUCT =======
+    // ======================
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+    
+    struct Person {
+        uint _id;
+        string _firstName;
+        string _lastName;
+    }
+    
+    function addPerson(string memory _firstName, string memory _lastName) public {
+        peopleCount += 1;
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
     }
 }
