5694f3b18c01fa1dd0e274c0cf06ec20e9d2e42b
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-06 22:28:23 +0100

    :sparkles: Solidity #3

--- solidity/MyContract.sol
+++ solidity/MyContract.sol
@@ -1,39 +1,26 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    // =======================
-    // ======== TYPES ========
-    // =======================
-    string public stringValue = "myString";
-    bool public myBool = true;
-    int public myInt = -1;
-    uint public myUint = 1;
-    uint8 public myUint8 = 10;
-    uint256 public myUint256 = 888889949;
-    
-    // ========================
-    // ========= ENUM =========
-    // ========================
-    enum State { Waiting, Ready, Active }
-    State public state;
-    
-    constructor() public {
-        state = State.Waiting;
-    }
-    
-    function activate() public {
-        state = State.Active;
-    }
-    
-    function isActive() public view returns(bool) {
-        return state == State.Active;
-    }
-
-    // ======================
-    // ======= STRUCT =======
-    // ======================
     uint256 public peopleCount = 0;
     mapping(uint => Person) public people;
+    
+    address owner;
+    
+    // create own modifier
+    modifier onlyOwner() {
+        // msg.sender : the person who execute this contract
+        // require : throw an error if the condition is false
+        require(msg.sender == owner);
+        _; // this will call the function owned by this modifier here
+    }
+    
+    uint256 openingTime = 1607291030; // Epoch timestamp
+    
+    modifier onlyWhileOpen() {
+        // block.timestamp : access time of the block in Epoch Unix Timestamp
+        require(block.timestamp >= openingTime);
+        _;
+    }
     
     struct Person {
         uint _id;
@@ -41,8 +28,32 @@ contract MyContract {
         string _lastName;
     }
     
-    function addPerson(string memory _firstName, string memory _lastName) public {
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    // onlyOwner modifier : onlyOwner can call this function
+    function addPerson(
+        string memory _firstName,
+        string memory _lastName)
+        public onlyOwner
+    {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+    
+    function incrementCount() internal {
         peopleCount += 1;
+    }
+    
+    // different modifier by time
+    function addPersonTime(
+        string memory _firstName,
+        string memory _lastName)
+        public onlyWhileOpen
+    {
+        incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
     }
 }
+
